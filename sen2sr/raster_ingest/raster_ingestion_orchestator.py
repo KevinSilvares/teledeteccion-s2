9563fb38b72135177fe07ae62fb24565ba50c0c6
KevinSilvares <k.silvares24@gmail.com>
2026-07-06 08:21:59 +0200

    Added tqdm when ingesting files and a few POIs

--- sen2sr/raster_ingest/raster_ingestion_orchestator.py
+++ sen2sr/raster_ingest/raster_ingestion_orchestator.py
@@ -1,5 +1,6 @@
 import os
 import logging
+from tqdm import tqdm
 from pathlib import Path
 from dotenv import load_dotenv
 
@@ -34,16 +35,45 @@ def main() -> None:
     client = CopernicusClient(client_id, client_secret)
 
     logger.info("Initializing Area of Interest grid.")
-    bboxes = aoi_gen.generate_grid_from_wgs84(
-        min_lon = -5.733359,
-        min_lat = 42.675830,
-        max_lon = -5.691903,
-        max_lat = 42.660368
-    )
+    pois = [
+        { # Leon
+            "min_lon": -6.423595,
+            "min_lat": 42.980884,
+            "max_lon": -6.238093,
+            "max_lat": 42.817713
+        },
+        { # Asturias
+            "min_lon": -6.379602,
+            "min_lat": 43.534458,
+            "max_lon": -6.229893,
+            "max_lat": 43.480966
+        },
+        { # Andalucia
+            "min_lon": -6.548216,
+            "min_lat": 38.000913,
+            "max_lon": -6.371529,
+            "max_lat": 37.877346
+        },
+        { # Almeria
+            "min_lon": -2.069388,
+            "min_lat": 37.034163,
+            "max_lon": -2.017125,
+            "max_lat": 37.011797
+        }
+    ]
+
+    bboxes = []
+    for poi in pois:
+        bboxes.extend(aoi_gen.generate_grid_from_wgs84(
+            min_lon = poi["min_lon"],
+            min_lat = poi["min_lat"],
+            max_lon = poi["max_lon"],
+            max_lat = poi["max_lat"]
+        ))
 
     dest = Path(__file__).resolve().parent.parent / "data" / "ingest"
 
-    for patch in bboxes:
+    for patch in tqdm(bboxes, desc = "Processing regionals grids"):
         unique_id = patch["id"]
         file_name = f"S2_patch_{unique_id}.tif"
 
